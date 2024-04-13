b5aafb5e71f18ff8b0c3a3c580f654f509d24dab
simoneca95 <simone.cantini95@hotmail.it>
2024-04-13 16:16:04 +0200

    base structure

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+
+    host: "http://localhost:8080",
+    //host: "https://deliveroojs.onrender.com",
+
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAyNTlmOWI3NjRmIiwibmFtZSI6InNpbW8iLCJpYXQiOjE3MTMwMTY5ODR9.q08-k9GdhdwUwPWFs_1iYQ_Y3I70OIGjLnBOAXHznc8'
+
+}
+
+export default config;
